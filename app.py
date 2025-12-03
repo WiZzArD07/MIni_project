a64a0745fd8ef80076d87bb32a06c472a5a063c8
Aryan Jaiswal <140463519+WiZzArD07@users.noreply.github.com>
2025-12-03 18:27:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import io
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import streamlit as st
 
